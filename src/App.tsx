4d8e016453974986fe1837c8b86007c5a1559525
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2025-03-06 20:16:30 +0530

    feat: implement StoresPage with ReorderableTable for store management

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LeftNavMenu, TopNavBar } from './components';
+import { AppLayout, LeftNavMenu, TopNavBar } from './components';
 import { ChartsPage, PlanningPage, SKUsPage, StoresPage } from './pages';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
 
   return (
     <Router>
-      <div className="flex flex-col">
-        <TopNavBar />
-        <div className="flex flex-1 mt-16">
-          <LeftNavMenu />
-          <Routes>
-            <Route path="/stores" element={<StoresPage />} />
-            <Route path="/skus" element={<SKUsPage />} />
-            <Route path="/planning" element={<PlanningPage />} />
-            <Route path="/charts" element={<ChartsPage />} />
-            <Route path="/" element={<StoresPage />} />
-          </Routes>
-        </div>
+      <TopNavBar />
+      <div className="flex flex-1 mt-16">
+        <LeftNavMenu />
+        <AppLayout >
+          <Provider store={store}>
+            <Routes>
+              <Route path="/stores" element={<StoresPage />} />
+              <Route path="/skus" element={<SKUsPage />} />
+              <Route path="/planning" element={<PlanningPage />} />
+              <Route path="/charts" element={<ChartsPage />} />
+              <Route path="/" element={<StoresPage />} />
+            </Routes>
+          </Provider>
+        </AppLayout>
       </div>
     </Router>
   )
